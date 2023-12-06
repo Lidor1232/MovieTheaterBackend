2772849ab84d7f9663482c162a729474224f89e6
lidordvir <lidor4001@gmail.com>
2023-12-06 12:30:55 +0200

    * Added _id to get movie schedule route

--- src/routes/controllers/movieSchedule/responses/get-movies-schedule-api-response.ts
+++ src/routes/controllers/movieSchedule/responses/get-movies-schedule-api-response.ts
@@ -3,6 +3,7 @@ import {Movie} from '../../../dto/movie.dto';
 
 export class GetMoviesScheduleApiResponse {
   moviesSchedule: {
+    _id: string;
     movie: MovieApiResponse;
     startDate: string;
     status: string;
@@ -12,12 +13,14 @@ export class GetMoviesScheduleApiResponse {
     moviesSchedule,
   }: {
     moviesSchedule: {
+      _id: string;
       movie: Movie;
       startDate: string;
       status: string;
     }[];
   }) {
     this.moviesSchedule = moviesSchedule.map(movieSchedule => ({
+      _id: movieSchedule._id,
       startDate: movieSchedule.startDate,
       status: movieSchedule.status,
       movie: new MovieApiResponse({
