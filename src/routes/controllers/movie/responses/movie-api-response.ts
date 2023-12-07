b6824303722d5aa4383de2de142c422d6e07521a
lidordvir <lidor4001@gmail.com>
2023-12-07 16:05:22 +0200

    * Added movie status in movie api response

--- src/routes/controllers/movie/responses/movie-api-response.ts
+++ src/routes/controllers/movie/responses/movie-api-response.ts
@@ -1,4 +1,4 @@
-import {Movie} from '../../../dto/movie.dto';
+import {Movie, MovieStatus} from '../../../dto/movie.dto';
 
 export class MovieApiResponse {
   _id: string;
@@ -9,10 +9,13 @@ export class MovieApiResponse {
 
   durationInMinutes: number;
 
+  status: MovieStatus;
+
   constructor({movie}: {movie: Movie}) {
     this._id = movie._id;
     this.title = movie.title;
     this.description = movie.description;
     this.durationInMinutes = movie.durationInMinutes;
+    this.status = movie.status;
   }
 }
